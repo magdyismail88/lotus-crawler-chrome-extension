af08f2120938dab3cfd52b6b6e574c97bb7fb3e9
Magdy Ismail <magdyismail8888@gmail.com>
2024-11-23 03:25:29 +0200

    updated content

--- content.js
+++ content.js
@@ -1,15 +1,27 @@
-function downloadAndNavigate() {
+async function downloadAndNavigate() {
     // Send HTML content to the background script for downloading
     chrome.runtime.sendMessage({
       action: "downloadHTML",
       htmlContent: document.documentElement.outerHTML
-    }, (response) => {
+    }, async (response) => {
       if (response && response.status === "Downloaded") {
+
+        await new Promise(resolve => {
+          // Scroll to the bottom of the page
+          window.scrollTo(0, document.body.scrollHeight);
+      
+          // Wait for 2 seconds after scrolling
+          setTimeout(resolve, 2000);
+        });
+
         // Check if there is a "next page" link
         const nextPageLink = document.querySelector('a[title="Go to next page"]');
         if (nextPageLink) {
           // Navigate to the next page
-          nextPageLink.click();
+          // nextPageLink.click();
+          window.location.href = nextPageLink.href;
+          await new Promise(resolve => setTimeout(resolve, 20000));
+          
         } else {
           console.log("No more pages to process.");
         }
